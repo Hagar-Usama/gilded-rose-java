b25f556c564800b23036e1a04f7035d2c3309f32
Hagar Usama <eng-hagar.usama1621@alexu.edu.eg>
2025-10-03 02:12:42 +0300

    refactor pass 7

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -7,45 +7,59 @@ class GildedRose {
     public GildedRose(Item[] items) {
         this.items = items;
     }
-
-
+    
     public void updateQuality() {
 
         for (Item item : items) {
 
             switch (item.name) {
                 case BACKSTAGE_PASSES:
-                    int increment = 0;
-                    if (item.sellIn < 11) increment++;
-                    if (item.sellIn < 6)  increment++;
-                    item.quality = Math.min(50, item.quality + increment);
-                    item.quality = Math.min(50, item.quality + 1);
-                    item.sellIn = item.sellIn - 1;
-                    if(item.sellIn < 0){
-                        item.quality = 0;
-                    }
+                    updateBackstagePasses(item);
                     break;
                 case AGED_BRIE:
-                    item.quality = Math.min(50, item.quality + 1);
-                    item.sellIn = item.sellIn - 1;
-                    if(item.sellIn < 0){
-                        item.quality = Math.min(50, item.quality + 1);
-                    }
+                    updateAgedBrie(item);
                     break;
                 case SULFURAS:
-                    item.quality = Math.max(80, item.quality);
+                    updateSulfuras(item);
                     break;
             
                 default:
-                    item.quality = Math.max(0, item.quality - 1);
-                    item.sellIn = item.sellIn - 1;
-                    if(item.sellIn < 0){
-                         item.quality = Math.max(0, item.quality - 1);
-                    }
-                    break;
+                    updateNormalItems(item);
             }
 
 
+        }
+    }
+
+    private  void updateNormalItems(Item item) {
+        item.quality = Math.max(0, item.quality - 1);
+        item.sellIn = item.sellIn - 1;
+        if(item.sellIn < 0){
+             item.quality = Math.max(0, item.quality - 1);
+        }
+    }
+
+    private void updateSulfuras(Item item) {
+        item.quality = Math.max(80, item.quality);
+    }
+
+    private void updateAgedBrie(Item item) {
+        item.quality = Math.min(50, item.quality + 1);
+        item.sellIn = item.sellIn - 1;
+        if(item.sellIn < 0){
+            item.quality = Math.min(50, item.quality + 1);
+        }
+    }
+
+    private void updateBackstagePasses(Item item) {
+        int increment = 0;
+        if (item.sellIn < 11) increment++;
+        if (item.sellIn < 6)  increment++;
+        item.quality = Math.min(50, item.quality + increment);
+        item.quality = Math.min(50, item.quality + 1);
+        item.sellIn = item.sellIn - 1;
+        if(item.sellIn < 0){
+            item.quality = 0;
         }
     }
 }
