f70ecd91473c1f73c382d59f75bd835ab5fc06cb
Hagar Usama <eng-hagar.usama1621@alexu.edu.eg>
2025-10-03 02:01:03 +0300

    refactor pass 6

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -8,39 +8,44 @@ class GildedRose {
         this.items = items;
     }
 
+
     public void updateQuality() {
 
         for (Item item : items) {
 
-            if(item.name.equals(BACKSTAGE_PASSES) ){
-                        int increment = 0;
-                        if (item.sellIn < 11) increment++;
-                        if (item.sellIn < 6)  increment++;
-                        item.quality = Math.min(50, item.quality + increment);
-                        item.quality = Math.min(50, item.quality + 1);
-                        item.sellIn = item.sellIn - 1;
-
-            }else if(item.name.equals(AGED_BRIE)){
-                item.quality = Math.min(50, item.quality + 1);
-                item.sellIn = item.sellIn - 1;
-            }else if(item.name.equals(SULFURAS)){
-                item.quality = Math.max(80, item.quality);
-            }else{
-
-                 item.quality = Math.max(0, item.quality - 1);
-                 item.sellIn = item.sellIn - 1;
-            }
-
-
-            if (item.sellIn < 0) {
-                if (item.name.equals(AGED_BRIE)) {
+            switch (item.name) {
+                case BACKSTAGE_PASSES:
+                    int increment = 0;
+                    if (item.sellIn < 11) increment++;
+                    if (item.sellIn < 6)  increment++;
+                    item.quality = Math.min(50, item.quality + increment);
                     item.quality = Math.min(50, item.quality + 1);
-                } else if (item.name.equals(BACKSTAGE_PASSES)) {
-                    item.quality = 0;
-                } else if (!item.name.equals(SULFURAS)) {
+                    item.sellIn = item.sellIn - 1;
+                    if(item.sellIn < 0){
+                        item.quality = 0;
+                    }
+                    break;
+                case AGED_BRIE:
+                    item.quality = Math.min(50, item.quality + 1);
+                    item.sellIn = item.sellIn - 1;
+                    if(item.sellIn < 0){
+                        item.quality = Math.min(50, item.quality + 1);
+                    }
+                    break;
+                case SULFURAS:
+                    item.quality = Math.max(80, item.quality);
+                    break;
+            
+                default:
                     item.quality = Math.max(0, item.quality - 1);
-                }
+                    item.sellIn = item.sellIn - 1;
+                    if(item.sellIn < 0){
+                         item.quality = Math.max(0, item.quality - 1);
+                    }
+                    break;
             }
+
+
         }
     }
 }
